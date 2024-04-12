8fa56483b80303ba51486b9b47ba8c5273d1871f
Rizalimaru <l200210119@student.ums.ac.id>
2024-04-12 20:23:07 +0700

    Update combat

--- IndonesiaInvaded/Assets/InputAction/Player/PlayerInputActions.cs
+++ IndonesiaInvaded/Assets/InputAction/Player/PlayerInputActions.cs
@@ -15,7 +15,7 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-namespace IndonesiaInvaded
+namespace GenshinImpactMovementSystem
 {
     public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
     {
