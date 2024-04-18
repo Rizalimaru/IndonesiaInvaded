89d17abbdc5f0e3f4dd19a48ea1806ca8d6e85e8
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-04-18 10:03:08 +0800

    update portal instant

---
+++ IndonesiaInvaded/Assets/Scripts/Teleport System/PortalInstant.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalInstant : MonoBehaviour
+{
+   [SerializeField] Transform destination;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if(other.CompareTag("Player") && other.TryGetComponent<PlayerController>(out var player)){
+        //     player.Teleport(destination.position, destination.rotation);
+        // }
+
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(destination.position, 4f);
+        var direction = destination.TransformDirection(Vector3.forward);
+        Gizmos.DrawRay(destination.position, direction);
+    }
+}
