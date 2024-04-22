d0b7c05981ee64a3df8322afb8d0bb8360630dcc
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-04-22 12:46:17 +0800

    .

--- IndonesiaInvaded/Assets/Scripts/Game Manager/PersistentData1.cs
+++ IndonesiaInvaded/Assets/Scripts/Game Manager/PersistentData1.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersistentData : MonoBehaviour
+public class PersistentData1 : MonoBehaviour
 {
-    public static PersistentData instance;
+    public static PersistentData1 instance;
 
     private void Awake()
     {
         if (instance != null)
         {
             Debug.Log("Instance already exists");
-            
+            Destroy(gameObject);
         }
             instance = this;
             DontDestroyOnLoad(gameObject);
