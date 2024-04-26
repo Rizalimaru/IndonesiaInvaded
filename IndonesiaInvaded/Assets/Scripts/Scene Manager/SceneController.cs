2758091bd39e1e66b21f26445789fd421ccf063c
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-04-26 10:41:53 +0800

    update

--- IndonesiaInvaded/Assets/Scripts/Scene Manager/SceneController.cs
+++ IndonesiaInvaded/Assets/Scripts/Scene Manager/SceneController.cs
@@ -13,7 +13,7 @@ public class SceneController : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+            // DontDestroyOnLoad(gameObject);
         }
         else
         {
