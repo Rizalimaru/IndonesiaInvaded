5a32ced6b9c7e4e75684c9751e2f2b4e7ccd64b9
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-05-08 18:49:27 +0800

    update

--- IndonesiaInvaded/Assets/Scripts/Scene Manager/Scene_Loading.cs
+++ IndonesiaInvaded/Assets/Scripts/Scene Manager/Scene_Loading.cs
@@ -33,7 +33,22 @@ public class Scene_Loading : MonoBehaviour
         
         StartCoroutine(LoadScenesAsync());
     } 
+    public void LoadScenes2()
+    {
+        loadingScreen.SetActive(true);
 
+        loadingBarFill.value = 0;
+        
+        StartCoroutine(LoadLevel2());
+    } 
+    public void LoadScenes3()
+    {
+        loadingScreen.SetActive(true);
+
+        loadingBarFill.value = 0;
+        
+        StartCoroutine(LoadLevel3());
+    } 
     IEnumerator LoadScenesAsync()
     {
         List<AsyncOperation> scenes = new List<AsyncOperation>();
@@ -41,6 +56,66 @@ public class Scene_Loading : MonoBehaviour
         // Sesuaikan indeks scene dengan indeks scene yang ingin Anda muat
         scenes.Add(SceneManager.LoadSceneAsync("Gameplay1"));
         scenes.Add(SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Additive));
+
+
+        // Tunggu hingga semua scene dimuat
+        foreach (var scene in scenes)
+        {
+            while (!scene.isDone)
+            {
+                float progress = 0;
+                foreach (var s in scenes)
+                {
+                    progress += s.progress;
+                }
+                progress /= scenes.Count;
+                loadingBarFill.value = progress;
+                yield return null;
+            }
+        }
+
+        // Tunggu sedikit waktu tambahan sebelum menonaktifkan layar loading
+        yield return new WaitForSeconds(1f);
+
+        loadingScreen.SetActive(false);
+    }
+    IEnumerator LoadLevel2()
+    {
+        List<AsyncOperation> scenes = new List<AsyncOperation>();
+
+        // Sesuaikan indeks scene dengan indeks scene yang ingin Anda muat
+        scenes.Add(SceneManager.LoadSceneAsync("Gameplay2"));
+        scenes.Add(SceneManager.LoadSceneAsync("Level2", LoadSceneMode.Additive));
+
+
+        // Tunggu hingga semua scene dimuat
+        foreach (var scene in scenes)
+        {
+            while (!scene.isDone)
+            {
+                float progress = 0;
+                foreach (var s in scenes)
+                {
+                    progress += s.progress;
+                }
+                progress /= scenes.Count;
+                loadingBarFill.value = progress;
+                yield return null;
+            }
+        }
+
+        // Tunggu sedikit waktu tambahan sebelum menonaktifkan layar loading
+        yield return new WaitForSeconds(1f);
+
+        loadingScreen.SetActive(false);
+    }
+    IEnumerator LoadLevel3()
+    {
+        List<AsyncOperation> scenes = new List<AsyncOperation>();
+
+        // Sesuaikan indeks scene dengan indeks scene yang ingin Anda muat
+        scenes.Add(SceneManager.LoadSceneAsync("Gameplay3"));
+        scenes.Add(SceneManager.LoadSceneAsync("Level3", LoadSceneMode.Additive));
 
 
         // Tunggu hingga semua scene dimuat
