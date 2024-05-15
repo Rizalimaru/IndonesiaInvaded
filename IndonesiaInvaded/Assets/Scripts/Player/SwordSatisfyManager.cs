71f1c917b51132961d8a660e6fb0a2c4fb5a5e93
Rizalimaru <l200210119@student.ums.ac.id>
2024-05-15 07:34:31 +0700

    Toning Combat

--- IndonesiaInvaded/Assets/Scripts/Player/SwordSatisfyManager.cs
+++ IndonesiaInvaded/Assets/Scripts/Player/SwordSatisfyManager.cs
@@ -1,28 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Serialization;
 using UnityEngine;
 
 public class SwordSatisfyManager : MonoBehaviour
-{
+{   
+    private Combat combat;
+    private bool isSlowMotionActive = false;
+
+    private void Awake()
+    {
+        combat = Combat.instance;
+    }
     private void OnTriggerEnter(Collider other)
     {
         // Periksa apakah objek yang masuk trigger memiliki tag "Enemy"
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy") && combat.isAttacking == true)
         {
-            StartCoroutine(StartSlowMotion());
+            StartCoroutine(slowMotionStart());
         }
     }
 
-    IEnumerator StartSlowMotion()
-    {   
-        Time.timeScale = 0.5f;
+    IEnumerator slowMotionStart()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Time.timeScale = 0.7f;
         yield return new WaitForSeconds(0.1f);
         Time.timeScale = 1f;
+
     }
 
-    void slowMotion()
-    {
-        Time.timeScale = 0.5f;
+    IEnumerator slowMotionStop()
+    {   
+        yield return new WaitForSeconds(0.1f);  
+        Time.timeScale = 1f;
     }
 
 
